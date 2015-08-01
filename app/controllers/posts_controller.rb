0c1fc197d40ddce2de14f9301947eff6249c12fc
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-08-01 02:28:20 -0500

    Add create and index actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    post = Post.create(title: params[:post][:title], content: params[:post][:content])
+    if post.valid?
+      redirect_to action: :index
+    end
+  end
 end
