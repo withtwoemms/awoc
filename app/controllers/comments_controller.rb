52ff5656d73248679b06e2f3d2e871ac527e53dd
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-08-02 16:45:46 -0500

    Create new comments from form params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,10 @@ class CommentsController < ApplicationController
   end
 
   def create
+  	comment = Comment.create(content: params[:comment][:content], post_id: params[:post_id])
+  	if comment.valid?
+  		redirect_to root_path
+  	end
   end
 
   def update
