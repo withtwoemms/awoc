cec3b574074d8d03a656f92414a2c6f8086c3025
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-08-02 16:43:03 -0500

    Make posts and comments available to view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    posts = Post.all
+    comments = posts.map(&:comments)
+    @posts_with_comments = posts.zip(comments)
   end
 
   def edit
