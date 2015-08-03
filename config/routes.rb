59e929bf69a632ea295a25b0c662c6de7aa5aff4
Emmanuel I. Obi <withtwoemms@gmail.com>
2015-08-02 20:20:37 -0500

    Nest post routes under topic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts do
-    resources :comments
+  resources :topics do 
+    resources :posts do
+      resources :comments
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
